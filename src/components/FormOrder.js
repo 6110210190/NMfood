754e5043dbc3d06cb9e7d252cbfc4f7400fdbcd6
6110210190 <6110210190@psu.ac.th>
2022-06-03 17:19:54 +0700

    main

--- src/components/FormOrder.js
+++ src/components/FormOrder.js
@@ -1,8 +1,20 @@
 import { Container, Button, Form } from 'react-bootstrap';
-import React from 'react';
 import Navbar from '../components/Navbar';
-function FormOrder() {
+import React, { useState, useEffect } from 'react';
+import { db } from "../firebase";
+import { collection, addDoc } from '@firebase/firestore';
 
+function FormOrder() {
+  const orderCollectionRef = collection(db, "order");
+
+  const [name, setName] = useState('');
+  const [address, setAddress] = useState('');
+  const [unit, setUnit] = useState(0);
+  const [tel, setTel] = useState('');
+
+  const createOrder = async () => {
+      await addDoc(orderCollectionRef, {name: name, address: address, unit: unit, tel: tel})
+  }
 
   return (
     <div>
@@ -10,18 +22,36 @@ function FormOrder() {
       <Container style= {{ padding: '3rem' }}>
         <h1 style={{textAlign: 'center', paddingBottom: '1rem'}}>Create Order</h1>
         <Form.Group className='mb-3'>
-          <Form.Control placeholder="Name"/>
+          <Form.Control 
+            placeholder="Name" 
+            onChange={(e) => {
+            setName(e.target.value);
+          }}/>
         </Form.Group>
         <Form.Group className='mb-3'>
-          <Form.Control placeholder="Address"/>
+          <Form.Control 
+            placeholder="Address"
+            onChange={(e) => {
+              setAddress(e.target.value);
+            }}/>
         </Form.Group>
         <Form.Group className='mb-3'>
-          <Form.Control placeholder="Unit"/>
+          <Form.Control 
+            placeholder="Unit"
+            onChange={(e) => {
+            setUnit(e.target.value);
+          }}/>
         </Form.Group>
         <Form.Group className='mb-3'>
-          <Form.Control placeholder="Telephone Number"/>
+          <Form.Control 
+            placeholder="Telephone Number"
+            onChange={(e) => {
+            setTel(e.target.value);
+          }}/>
         </Form.Group>
-        <Button variant="primary" type="submit" style={{display: 'block', marginLeft: 'auto', marginRight: 'auto' }}>
+        <Button variant="primary" type="submit" style={{display: 'block', marginLeft: 'auto', marginRight: 'auto' }}
+          onClick={createOrder}
+        >
           Submit
         </Button>
       </Container>
