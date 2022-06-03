3a92b6a70cab8b8b1b735ad77562a0985edde6e4
6110210190 <6110210190@psu.ac.th>
2022-06-03 16:40:41 +0700

    main

--- src/components/ShowOrder.js
+++ src/components/ShowOrder.js
@@ -2,38 +2,50 @@ import { Container } from 'react-bootstrap';
 import React, { useState, useEffect } from 'react';
 import Navbar from '../components/Navbar';
 import { db } from "../firebase";
-import { collection, getDocs, order } from '@firebase/firestore';
+import { collection, getDocs } from '@firebase/firestore';
+import { Card, Button } from 'react-bootstrap';
 
 
+function ShowOrder() {
+  const [order , setOrder] = useState ([]);
+  const orderCollectionRef = collection(db, "order");
 
-function FormOrder() {
-
-  // const [name, setName] = useState([])
-  // const nameCollectionRef = collection(db, "name");
-  // useEffect(() => {
-  //   const getName = async () => {
-  //        const data = await getDocs(nameCollectionRef);
-  //        setName(data.docs.map((doc) => ({...doc.data(), id: doc.id})))
-  //   };
-  //   getName();
-  // }, [])
+  useEffect (() => {
+    const getOrder = async() => {
+      const data = await getDocs(orderCollectionRef);
+      setOrder(data.docs.map((doc) => ({...doc.data(), id: doc.id})));
+    };
+    getOrder();
+  }, []);
   return (
     <div>
       <Navbar />
       <Container style= {{ padding: '3rem' }}>
         <h1 style={{textAlign: 'center', paddingBottom: '1rem'}}>Order List</h1>
-        {/* {order.map((order) => {
+        {order.map((order) => {
           return (
             <div>
               {" "}
-              name: {order.name}
+              <Card style={{ width: '18rem'}}>
+                <Card.Body>
+                  <Card.Title>Customer: {order.name}</Card.Title>
+                  <Card.Subtitle className="mb-2 text-muted">{order.address}</Card.Subtitle>
+                  <Card.Text>
+                    Unit: {order.unit} <br/>
+                    tel: {order.tel} 
+                  </Card.Text>
+                  <Button variant="primary" type="submit" style={{display: 'block', marginLeft: 'auto', marginRight: 'auto' }}>
+                    complete
+                  </Button>
+                </Card.Body>
+              </Card>
             </div>
-          )
-        })} */}
+          );
+        })}
       </Container>
        
     </div>
   );
 }
 
-export default FormOrder;
+export default ShowOrder;
