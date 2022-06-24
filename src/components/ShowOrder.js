b8046992bf393c4b9015c592c1cd62fbd74beeb9
6110210190 <6110210190@psu.ac.th>
2022-06-24 11:37:20 +0700

    Update ShowOrder.js

--- src/components/ShowOrder.js
+++ src/components/ShowOrder.js
@@ -12,7 +12,13 @@ import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 import Button from '@mui/material/Button';
-import VisibilityOutlinedIcon from '@mui/icons-material/VisibilityOutlined';
+import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
+import Backdrop from '@mui/material/Backdrop';
+import CircularProgress from '@mui/material/CircularProgress';
+import Card from '@mui/material/Card';
+import CardActionArea from '@mui/material/CardActionArea';
+import Typography from '@mui/material/Typography';
+import CardContent from '@mui/material/CardContent';
 
 function ShowOrder() {
 
@@ -27,6 +33,14 @@ function ShowOrder() {
     []
   );
   
+  const [open, setOpen] = useState(false);
+  const handleClose = () => {
+    setOpen(false);
+  };
+  const handleToggle = () => {
+    setOpen(!open);
+  };
+
   
   // const handleDelete = async (id) => {
   //   const orderDoc = doc(db, "order", id);
@@ -36,17 +50,17 @@ function ShowOrder() {
   return (
     <div>
       <Menu/>
-      <div style={{margin: '20px'}}>
+      <div style={{margin: '30px'}}>
         <TableContainer component={Paper}>
             <Table sx={{ minWidth: 650 }} size="medium" aria-label="a dense table">
               <TableHead>
                 <TableRow>
                   <TableCell align='center' ><b>No.</b></TableCell>
-                  <TableCell align='center'><b>Name</b></TableCell>
-                  <TableCell align='center'><b>Address</b></TableCell>
+                  <TableCell align='center'><b>Customer Name</b></TableCell>
+                  <TableCell align='center'><b>Customer Address</b></TableCell>
                   <TableCell align='center'>
                     <b>
-                      Action
+                      See more
                     </b>
                   </TableCell>
                 </TableRow>
@@ -60,14 +74,33 @@ function ShowOrder() {
                     <TableCell align='center' component="th" scope="row">
                       {order.no}
                     </TableCell>
-                    <TableCell align='left'>{order.name}</TableCell>
-                    <TableCell align='left'>{order.address}</TableCell>
+                    <TableCell align='center'>{order.name}</TableCell>
+                    <TableCell align='center'>{order.address}</TableCell>
                     <TableCell align='center'>
                       <Button>
-                        <VisibilityOutlinedIcon color="primary">
+                        <MoreHorizIcon color="primary" onClick={handleToggle}>
                           {/* see more code */}
-                        </VisibilityOutlinedIcon>
+                        </MoreHorizIcon>
                       </Button>
+                      <Backdrop
+                        sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
+                        open={open}
+                        onClick={handleClose}
+                      >
+                        {/* <CircularProgress color="inherit" /> */}
+                        <Card sx={{ maxWidth: 345 }}>
+                          <CardActionArea>
+                          <CardContent>
+                            <Typography gutterBottom variant="h5" component="div">
+                              Data more
+                            </Typography>
+                            <Typography variant="body2" color="text.secondary">
+                              Data more
+                            </Typography>
+                          </CardContent>
+                          </CardActionArea>
+                        </Card>
+                      </Backdrop>
                     </TableCell>
                   </TableRow>
                 ))}
