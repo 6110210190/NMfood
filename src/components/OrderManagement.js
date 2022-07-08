15b20bbb849df8f8d1009fdad936de9be12a5cae
6110210190 <6110210190@psu.ac.th>
2022-07-08 09:15:52 +0700

    Update OrderManagement.js

--- src/components/OrderManagement.js
+++ src/components/OrderManagement.js
@@ -105,6 +105,7 @@ function OrderManagement() {
                     sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
                   >
                     <TableCell align='center'><h5>{order.name}</h5></TableCell>
+                    <TableCell align='center'><h5>{order.address}</h5></TableCell>
                     <TableCell align='center'><h5>{order.unit}</h5></TableCell>
                     <TableCell align='center'>
                     <ButtonGroup size="small" aria-label="small button group">
